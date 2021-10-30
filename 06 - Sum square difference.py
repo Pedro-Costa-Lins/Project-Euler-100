ccbcbacf37823573aeeb4bf46efa94504714b79f
Pedro Costa de Albuquerque Lins <46895490+Pedro-Costa-Lins@users.noreply.github.com>
2021-10-30 10:40:17 -0300

    Update 06 - Sum square difference.py

--- 06 - Sum square difference.py
+++ 06 - Sum square difference.py
@@ -14,17 +14,16 @@
 # Find the difference between the sum of the squares of the 
 # first one hundred natural numbers and the square of the sum.
 
-def sumSquareDifference():
+def sumSquareDifference(n):
     SumSqr = 0
     SqrSum = 0
     
-    for i in range(1, 101):
+    for i in range(1, n + 1):
         SumSqr = SumSqr + (i ** 2)
         SqrSum = SqrSum + i
     
     SqrSum = SqrSum ** 2
-
     
     return SqrSum - SumSqr
 
-print(sumSquareDifference())
+print(sumSquareDifference(100))
