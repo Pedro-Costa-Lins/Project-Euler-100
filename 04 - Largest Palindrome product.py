f9f3ffee04f02f4659246f5710104b79f88fce6b
Pedro Costa de Albuquerque Lins <46895490+Pedro-Costa-Lins@users.noreply.github.com>
2021-10-30 12:18:52 -0300

    Update 04 - Largest Palindrome product.py

--- 04 - Largest Palindrome product.py
+++ 04 - Largest Palindrome product.py
@@ -1,38 +1,37 @@
+'''
 #A palindromic number reads the same both ways. The largest palindrome made from the 
 #product of two 2-digit numbers is 9009 = 91 × 99.
 
 #Find the largest palindrome made from the product of two 3-digit numbers.
+'''
 import math
 
 def isPalindrome(number):
+    '''Tests the given integer and if it is an palindrome'''
+    
     digits = str(number)
-    palin = True
 
     #Compares the first digit to the last, the second to the before last an so on.
     for i in range(1, math.floor( len( digits ) /2 ) + 1 ):
+        
         if digits[-i] != digits[i-1]:
-            #Declares 'Palin' = False if the number is not a Palindrome.
-            palin = False
-            break
-    
-    #Returns the 'palin' boolean.
-    return palin
+            return False 
+
+    return True
 
 def biggestPalindromeUntil(number):
-    nine = '9'
-    until = ''
+    '''Receives the intruction of how many digit numbers are going to be tested'''
+
+    nine = 0.99999999999999 #Some quick setting of the range expected.
+    ProductRange = int(math.ceil(nine * 10 ** number))
+    
     Biggest = 1
 
-    for i in range(number):
-        until = until + nine
-    ProductRange = int(until)
-
-    for i in range(100, ProductRange):
+    for i in range(int(ProductRange / 10), ProductRange):
         for j in range(i, ProductRange):
             if isPalindrome(i * j) and i * j > Biggest:
                 Biggest = i * j
-    
+
     return Biggest
 
 print(biggestPalindromeUntil(3))
-
