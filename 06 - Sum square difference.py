51bffe21a7171b33408258f913e7b5be46bd4975
Pedro Costa de Albuquerque Lins <46895490+Pedro-Costa-Lins@users.noreply.github.com>
2021-11-20 12:27:23 -0300

    Update 06 - Sum square difference.py

--- 06 - Sum square difference.py
+++ 06 - Sum square difference.py
@@ -15,6 +15,8 @@ first ten natural numbers and the square of the sum is:
 Find the difference between the sum of the squares of the 
 first one hundred natural numbers and the square of the sum.
 """
+
+
 def sum_square_difference(n):
     sumSqr = 0
     sqrSum = 0
