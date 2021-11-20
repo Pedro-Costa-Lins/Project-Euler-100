5d4888d907a615e8e54e00e04ca18f6560d598b4
Pedro Costa de Albuquerque Lins <46895490+Pedro-Costa-Lins@users.noreply.github.com>
2021-11-20 00:58:15 -0300

    Update 09 - Special Pythagorean triplet.py

--- 09 - Special Pythagorean triplet.py
+++ 09 - Special Pythagorean triplet.py
@@ -1,25 +1,26 @@
-# A Pythagorean triplet is a set of three natural
-# numbers, a < b < c, for which,
+""" Special Pythagorean
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which:
 
-# a² + b² = c²
+a² + b² = c²
 
-#For example, 3² + 4² = 9 + 16 = 25 = 5².
+For example, 3² + 4² = 9 + 16 = 25 = 5².
 
-# There exists exactly one Pythagorean triplet for
-# which a + b + c = 1000.
+There exists exactly one Pythagorean triplet for which:
 
-# Find the product abc. 
+a + b + c = 1000.
 
-def specialPythagorean(n):
+Find the product abc. 
+"""
+def special_pythagorean(n):
     
     for i in range(2, n):
         for j in range(i+1, n):
             for k in range(j+1, n):
-                if i + j + k == 1000 and i ** 2 + j ** 2 == k ** 2:
+                if i+j+k == 1000 and i**2 + j**2 == k**2:
                     
                     return f'Numbers: {i} {j} and {k}. Sum: {i+j+k}. Product: {i*j*k}.'
 
     return f'Not found matches for {n}.'
 
-print(specialPythagorean(1000))
-
+Number = 1000
+print(special_pythagorean(Number))
