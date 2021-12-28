d8ae4d480b7a0022c830fc42890cb25eab274325
Pedro Costa de Albuquerque Lins <46895490+Pedro-Costa-Lins@users.noreply.github.com>
2021-12-28 11:38:03 -0300

    Update decorators.py

--- Support Tools/decorators.py
+++ Support Tools/decorators.py
@@ -1,26 +1,41 @@
-import time
-
-
 def time_it(func):
+    import time
+
     def wrapper(*args, **kwargs):
         print(f'Start of {func.__name__}.')
         print(f'Description:\n\t{func.__doc__}', end='\n\n')
         
         # Will only time the function itself, hopefully.
+        before = time.time()
         try:
-            before = time.time()
             val = func(*args, **kwargs)
+            
+        except Exception:
+            val = f'Error in ({func.__name__}) or invalid input.'
+        except KeyboardInterrupt:
+            val = 'User interrupted.'
+        finally:
             time_taken = time.time() - before
-        
+            
             # Nice presentation.
             print(f'Result: {val}\nTime:   {time_taken} seconds.')
-        
+            print(f'\nEnd of ({func.__name__}) function.')
+                
+    return wrapper
+
+
+def log(func):
+    import logging
+    logging.basicConfig(filename=f'{func.__name__}.log', level=logging.INFO)
+    def wrapper(*args, **kwargs):
+        try:
+            val = func(*args, **kwargs)
         except Exception:
-            print(f'There was an error, invalid input given or ({func.__name__}) is not working.')
+            val = f'Error in {func.__name__} or invalid input.'
         except KeyboardInterrupt:
-            print('WHY? Did you do something wrong again?')
+            val = 'User interrupted.'
         finally:
-            print(f'\nEnd of {func.__name__}.')
-        
-        
+            logging.info(f'Ran with args: {args}, and kwargs: {kwrags}')
+            return val
+    
     return wrapper
