29f7f464eec1fc18b512a5993ef1eaa9735c2ab0
Pedro Costa de Albuquerque Lins <46895490+Pedro-Costa-Lins@users.noreply.github.com>
2022-01-03 16:57:25 -0300

    Docstrings getting owned

--- 1 _ 1 to 5/Problem 1 - Multiples of 3 and 5/Problem_001.py
+++ 1 _ 1 to 5/Problem 1 - Multiples of 3 and 5/Problem_001.py
@@ -8,7 +8,8 @@ class problem_1(problem):
 
     The sum of these multiples is 23.
 
-    Find the sum of all the multiples of 3 or 5 below 1000."""
+    Find the sum of all the multiples of 3 or 5 below 1000.
+    """
 
     # === == === Solution === == ===  #
 
--- 1 _ 1 to 5/Problem 4 - Largest Palindrome product/Largest Palindrome product.py
+++ 1 _ 1 to 5/Problem 4 - Largest Palindrome product/Largest Palindrome product.py
@@ -1,27 +1,32 @@
-""" Largest Palindrome
-A palindromic number reads the same both ways. The largest palindrome made from the 
-product of two 2-digit numbers is 9009 = 91 × 99.
-
-Find the largest palindrome made from the product of two 3-digit numbers.
-"""
 import math
 
 
 def isPalindrome(number):
-    
+    """Compares the digits until finding a difference, if it does,
+    then it's not palindrome.
+    """
+
     digits = str(number)
 
-    #Compares the first digit to the last, the second to the before last an so on.
-    for i in range(1, math.floor( len( digits ) /2 ) + 1 ):
+    # Compares the first digit to the last, the second to the before
+    # the last an so on.
+    for i in range(1, math.floor(len(digits) /2) +1):
         if digits[-i] != digits[i-1]:
             return False
 
     return True
 
 
-def biggest_palindrome_with_n_digits(number):
+def biggest_palindrome_with_n_digits(number=3):
     
-    #Setting of the range expected.
+    """ Largest Palindrome
+    A palindromic number reads the same both ways. The largest palindrome made from the 
+    product of two 2-digit numbers is 9009 = 91 × 99.
+
+    Find the largest palindrome made from the product of two 3-digit numbers.
+    """
+
+    # Setting of the range expected.
     nine = 0.99999999999999 
     ProductRange = int(math.ceil(nine * 10 ** number))
     
@@ -34,4 +39,5 @@ def biggest_palindrome_with_n_digits(number):
 
     return Biggest
 
-print(biggest_palindrome_with_n_digits(3))
+
+print(biggest_palindrome_with_n_digits())
