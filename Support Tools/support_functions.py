9ba664d9b4d8f43a5e1ce3091bfd391f55bbd631
Pedro Costa de Albuquerque Lins <46895490+Pedro-Costa-Lins@users.noreply.github.com>
2022-01-03 16:14:58 -0300

    update and new support function

--- Support Tools/support_functions.py
+++ Support Tools/support_functions.py
@@ -1,9 +1,9 @@
-from math import ceil, sqrt
-
 def is_prime(number):
-    
+    from math import ceil, sqrt
+
     for i in range(3, ceil(sqrt(number))):
         if not(n % i):
             return False
     
     return True
+
