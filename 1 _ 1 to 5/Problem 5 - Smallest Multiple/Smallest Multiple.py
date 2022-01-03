3a57feba6f5a6a0941b3c9fd519e860aa1f11385
Pedro Costa de Albuquerque Lins <46895490+Pedro-Costa-Lins@users.noreply.github.com>
2022-01-03 17:06:13 -0300

    update

--- 1 _ 1 to 5/Problem 5 - Smallest Multiple/Smallest Multiple.py
+++ 1 _ 1 to 5/Problem 5 - Smallest Multiple/Smallest Multiple.py
@@ -1,16 +1,3 @@
-""" Smallest Multiple
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-
-20, 19, 18, 17, 16, 15, 14, 13, 12, 11 <- only numbers to worry about
-
-# Thoughts:
-    Should be even (Throught iterations of an even number it is resolved)
-    Should finish with 20, 40, 60, 80 or 00 (Throught iterations of 20 it is resolved)
-"""
-
-
 def is_multiple(n, numbers):
 
     #If the 'n' is not multiple, it's ruled out.
@@ -20,7 +7,22 @@ def is_multiple(n, numbers):
     return True
 
 
-def small_multiple(numbers):
+def small_multiple(numbers=[11, 20]):
+
+    """ Smallest Multiple
+    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+    20, 19, 18, 17, 16, 15, 14, 13, 12, 11 <- only numbers to worry about
+    """
+
+    # Thoughts:
+    #    Should be even (Throught iterations of an even number it is resolved)
+    #    Should finish with 20, 40, 60, 80 or 00 (Throught iterations of 20 it is resolved)
+
+    # === == === Solution === == ===  #
+
     notFound = True
     notOne = False
     iteration = 20
@@ -34,4 +36,4 @@ def small_multiple(numbers):
     return n
     
     
-print(small_multiple(numbers=[11, 20]))
+print(small_multiple(numbers))
