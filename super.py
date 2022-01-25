b190666fe06e27746229f71f8c913d52286c9873
Pedro Costa de Albuquerque Lins <46895490+Pedro-Costa-Lins@users.noreply.github.com>
2022-01-25 11:11:34 -0300

    Optimizing

--- super.py
+++ super.py
@@ -13,15 +13,6 @@ class Problem:
 
         pass
 
-    # def show_code(self):  # Not good, at least not this way.
-    #
-    #     """Fetches a file called 'plain_code.txt' present
-    #     in the file of every problem, then displays it."""
-    #
-    #     with open('plain_code.txt', 'r') as f:
-    #         print(f.read())
-
     def __init__(self):
         if __name__ == "__main__":
-            print('Executing')
             print(self.solution())
